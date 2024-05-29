60d9239198daf2814e2af7b324fd29b9b816de67
Zachary J. Rollyson <zach@adxsoftware.com>
2024-05-28 23:20:16 -0400

    trying out misc packages…
    …probably some i don't need :)

--- init.el
+++ init.el
@@ -34,18 +34,18 @@
        vertico           ; the search engine of the future
 
        :ui
-       ;;deft              ; notational velocity for Emacs
+       deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
 					;(emoji +unicode) ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       ;;hydra
+       hydra
        indent-guides     ; highlighted indent columns
        ligatures         ; ligatures and symbols to make your code pretty again
-       ;;minimap           ; show a map of the code on the side
+       minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
-       ;;nav-flash         ; blink cursor line after big motions
+       nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
@@ -68,7 +68,7 @@
        multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
-       ;;rotate-text       ; cycle region at point between text candidates
+       rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
        word-wrap         ; soft wrapping with language-aware indent
 
@@ -159,7 +159,14 @@
        ;;nim					; python + lisp at the speed of c
        ;;nix					; I hereby declare "nix geht mehr!"
        ;;ocaml					; an objective camel
-       (org +hugo)				; organize your plain life in plain text
+       (org					; organize your plain life in plain text
+	+hugo
+	+dragndrop
+	+journal
+	+pomodoro
+	+pretty
+	+roam2
+	)
        ;;php					; perl's insecure younger brother
        ;;plantuml				; diagrams for confusing people more
        ;;purescript				; javascript, but functional
