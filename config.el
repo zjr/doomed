112a75e1f477e611ab931811e9201a6b6bf111a4
Zachary J. Rollyson <zach@adxsoftware.com>
2024-05-28 23:09:51 -0400

    trying out iosevka font

--- config.el
+++ config.el
@@ -28,7 +28,7 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 13))
+(setq doom-font (font-spec :family "Iosevka Term Curly" :size 13 :weight 'regular :width 'expanded))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
